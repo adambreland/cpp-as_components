bf048216d3414d39132853fe7e2f7faadddfc220
Adam Breland <adam.j.breland@gmail.com>
2020-06-25 15:29:03 -0400

    Finished constructor exception test cases for FCGIServerInterface.

--- test/fcgi_si_test.cc
+++ test/fcgi_si_test.cc
@@ -7,6 +7,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/uio.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include <algorithm>
@@ -2976,13 +2977,12 @@ TEST(FCGIServerInterface, ConstructionExceptions)
   // 
   // Properties which should not cause a throw:
   // ("false positive" or "true negative" determination: EXPECT_NO_THROW)
-  // 1) Value of listening_descriptor: zero, non-zero.
-  // 2) Maximum value of max_connections.
-  // 3) Maximum value of max_requests.
-  // 4) A non-default value for app_status_on_abort.
-  // 5) An internet domain socket which either has FCGI_WEB_SERVER_ADDRS
+  // 1) Maximum value of max_connections.
+  // 2) Maximum value of max_requests.
+  // 3) A non-default value for app_status_on_abort.
+  // 4) An internet domain socket which either has FCGI_WEB_SERVER_ADDRS
   //    unbound or bound and empty.
-  // 6) A Unix domain socket:
+  // 5) A Unix domain socket:
   //    a) Where FCGI_WEB_SERVER_ADDRS is unbound.
   //    b) Where FCGI_WEB_SERVER_ADDRS is bound to internet addresses.
   //
@@ -2995,7 +2995,8 @@ TEST(FCGIServerInterface, ConstructionExceptions)
   //  4) The socket is of domain AF_INET and only IPv6 addresses are present.
   //  5) The socket is of domain AF_INET6 and only IPv4 addresses are present.
   //  6) The socket is of domain AF_INET and a combination of invalid IPv4
-  //     addresses and IPv6 addresses are present. "Invalid" means malformed.
+  //     addresses and valid IPv6 addresses are present. "Invalid" means
+  //     malformed.
   //  7) The socket is of domain AF_INET and only a comma is present.
   //  8) max_connections == -1.
   //  9) max_connections == 0.
@@ -3004,10 +3005,9 @@ TEST(FCGIServerInterface, ConstructionExceptions)
   // 12) An interface already exists and another call to the constructor is
   //     made. The arguments to the second call are the same as the first.
   // Throw not expected:
-  // 13) listening_descriptor == 0. The descriptor is a valid socket.
-  //     FCGI_WEB_SERVER_ADDRS is unbound.
-  // 14) listening_descriptor != 0. The descriptor is a valid socket.
-  //     FCGI_WEB_SERVER_ADDRS is bound and empty.
+  // 13) FCGI_WEB_SERVER_ADDRS is unbound. The descriptor is a valid socket.
+  // 14) FCGI_WEB_SERVER_ADDRS is bound and empty. The descriptor is a valid
+  //     socket.
   // 15) max_connections == std::numeric_limits<int>::max() &&
   //     max_requests    == std::numeric_limits<int>::max()
   //     Also, a non-default value is provided for app_status_on_abort.
@@ -3019,10 +3019,15 @@ TEST(FCGIServerInterface, ConstructionExceptions)
   //
   // Other modules whose testing depends on this module: none.
 
+  auto ClearFCGIWebServerAddrs = []()->void
+  {
+    if(setenv("FCGI_WEB_SERVER_ADDRS", "", 1) < 0)
+      FAIL() << "setenv failed" << '\n' << strerror(errno);
+  };
+
   // Ensure that FCGI_WEB_SERVER_ADDRS is bound and empty to establish a
   // consistent start state.
-  if(setenv("FCGI_WEB_SERVER_ADDRS", "", 1) < 0)
-    FAIL() << "setenv failed" << '\n' << strerror(errno);
+  ClearFCGIWebServerAddrs();
 
   // Case 1: listening_descriptor refers to a file which is not a socket.
   // Create a temporary regular file.
@@ -3051,10 +3056,9 @@ TEST(FCGIServerInterface, ConstructionExceptions)
       if(bind(socket_fd, static_cast<struct sockaddr*>(static_cast<void*>(&sa)),
         sizeof(struct sockaddr_in)) < 0)
       { 
-        int errno_value {errno};
-        close(socket_fd);
         ADD_FAILURE() << "A call to bind failed in case 2." << '\n' 
-          << strerror(errno_value);
+          << strerror(errno);
+        close(socket_fd);
       }
       else
       {
@@ -3084,10 +3088,9 @@ TEST(FCGIServerInterface, ConstructionExceptions)
       if(bind(socket_fd, static_cast<struct sockaddr*>(static_cast<void*>(&sa)),
         sizeof(struct sockaddr_in)) < 0)
       { 
-        int errno_value {errno};
-        close(socket_fd);
         ADD_FAILURE() << "A call to bind failed in case 3." << '\n' 
-          << strerror(errno_value);
+          << strerror(errno);
+        close(socket_fd);
       }
       else
       {
@@ -3098,58 +3101,266 @@ TEST(FCGIServerInterface, ConstructionExceptions)
     }
   }
 
-  // Case 4: The socket is of domain AF_INET and only IPv6 addresses are
-  // present.
+  auto FCGIWebServerAddrsCase = 
+    [&ClearFCGIWebServerAddrs](const char* address_list_ptr, int domain, 
+      int test_case)->void
   {
-    if(setenv("FCGI_WEB_SERVER_ADDRS", "::1", 1) < 0)
-      ADD_FAILURE() << "setenv failed in case 4." << '\n' << strerror(errno);
+    std::string case_suffix {" case "};
+    case_suffix += std::to_string(test_case);
+    case_suffix += ".";
+
+    if(setenv("FCGI_WEB_SERVER_ADDRS", address_list_ptr, 1) < 0)
+      ADD_FAILURE() << "setenv failed in" << case_suffix << '\n' 
+        << strerror(errno);
     else
     {
-      int socket_fd {socket(AF_INET, SOCK_STREAM, 0)};
+      int socket_fd {socket(domain, SOCK_STREAM, 0)};
       if(socket_fd < 0)
       {
-        ADD_FAILURE() << "A call to socket failed in case 4." << '\n' 
+        ADD_FAILURE() << "A call to socket failed in" << case_suffix << '\n' 
           << strerror(errno);
+        ClearFCGIWebServerAddrs();
       }
       else
       {
         if(listen(socket_fd, 5) < 0)
         {
-          int errno_value {errno};
+          ADD_FAILURE() << "A call to listen failed in" << case_suffix << '\n'
+            << strerror(errno);
           close(socket_fd);
-          ADD_FAILURE() << "A call to listen failed in case 4." << '\n'
-            << strerror(errno_value);
+          ClearFCGIWebServerAddrs();
         }
         else
         {
           EXPECT_THROW(fcgi_si::FCGIServerInterface(socket_fd, 1, 1),
             std::exception);
           close(socket_fd);
+          ClearFCGIWebServerAddrs();
         }
+      }
+    }
+  };
+
+  // Case 4: The socket is of domain AF_INET and only IPv6 addresses are
+  // present.
+  FCGIWebServerAddrsCase("::1", AF_INET, 4);
+
+  // Case 5: The socket is of domain AF_INET6 and only IPv4 addresses are
+  // present.
+  FCGIWebServerAddrsCase("127.0.0.1", AF_INET6, 5);
+  
+  // Case 6: The socket is of domain AF_INET and a combination of invalid IPv4
+  // addresses and valid IPv6 addresses are present. "Invalid" means malformed.
+  FCGIWebServerAddrsCase("::,127.0.0.256,::1,0.0.0.0.0", AF_INET, 6);
+
+  // Case 7: The socket is of domain AF_INET and only a comma is present.
+  FCGIWebServerAddrsCase(",", AF_INET, 7);
+
+  auto MaxConnectionsMaxRequestsCase = [](int max_connections,
+    int max_requests, int test_case)->void
+  {
+    std::string case_suffix {" case "};
+    case_suffix += std::to_string(test_case);
+    case_suffix += ".";
+
+    int socket_fd {socket(AF_INET, SOCK_STREAM, 0)};
+    if(socket_fd < 0)
+    {
+      ADD_FAILURE() << "A call to socket failed in" << case_suffix << '\n' 
+        << strerror(errno);
+    }
+    else
+    {
+      if(listen(socket_fd, 5) < 0)
+      { 
+        ADD_FAILURE() << "A call to listen failed in" << case_suffix << '\n' 
+          << strerror(errno);
+        close(socket_fd);
+      }
+      else
+      {
+        EXPECT_THROW(fcgi_si::FCGIServerInterface(socket_fd, max_connections, 
+          max_requests), std::exception);
+        close(socket_fd);
+      }
+    }
+  };
+
+  // Case 8: max_connections == -1.
+  MaxConnectionsMaxRequestsCase(-1, 1, 8);
+  
+  // Case 9: max_connections == 0.
+  MaxConnectionsMaxRequestsCase(0, 1, 9);
+  
+  // Case 10: max_requests == -1.
+  MaxConnectionsMaxRequestsCase(1, -1, 10);
+
+  // Case 11: max_requests == 0. 
+  MaxConnectionsMaxRequestsCase(1, 0, 11);
+
+  // 12) An interface already exists and another call to the constructor is
+  //     made. The arguments to the second call are the same as the first.
+  {
+    int socket_fd {socket(AF_INET, SOCK_STREAM, 0)};
+    if(socket_fd < 0)
+    {
+      ADD_FAILURE() << "A call to socket failed in case 12." << '\n' 
+        << strerror(errno);
+    }
+    else
+    {
+      if(listen(socket_fd, 5) < 0)
+      { 
+        ADD_FAILURE() << "A call to listen failed in case 12." << '\n' 
+          << strerror(errno);
+        close(socket_fd);
+      }
+      else
+      {
+        fcgi_si::FCGIServerInterface interface {socket_fd, 1, 1};
+        EXPECT_THROW(fcgi_si::FCGIServerInterface(socket_fd, 1, 1), 
+          std::exception);
+        close(socket_fd);
       }
     }
   }
 
-  // Case 5: 
+  // Throw not expected:
 
+  auto ValidSocketCase = [](int domain, int max_connections, int max_requests,
+    std::int32_t app_status_on_abort, int test_case)->void
+  {
+    std::string case_suffix {" case "};
+    case_suffix += std::to_string(test_case);
+    case_suffix += ".";
 
+    int socket_fd {socket(domain, SOCK_STREAM, 0)};
+    if(socket_fd < 0)
+    {
+      ADD_FAILURE() << "A call to socket failed in" << case_suffix << '\n' 
+        << strerror(errno);
+    }
+    else
+    {
+      // An unnamed UNIX socket is used. A call to bind is not necessary.
+      if(listen(socket_fd, 5) < 0)
+      { 
+        ADD_FAILURE() << "A call to listen failed in" << case_suffix << '\n' 
+          << strerror(errno);
+        close(socket_fd);
+      }
+      else
+      {
+        EXPECT_NO_THROW(fcgi_si::FCGIServerInterface(socket_fd, 1, 1, 
+          app_status_on_abort));
+        close(socket_fd);
+      }
+    }
+  };
 
+  // Case 13: FCGI_WEB_SERVER_ADDRS is unbound. The descriptor is a valid
+  // socket.
+  {
+    if(unsetenv("FCGI_WEB_SERVER_ADDRS") < 0)
+      ADD_FAILURE() << "FCGI_WEB_SERVER_ADDRS could not be removed from the "
+        "environment in case 13.";
+    else
+    {
+      ValidSocketCase(AF_INET, 1, 1, EXIT_FAILURE, 13);
+      ClearFCGIWebServerAddrs();
+    }
+  }
 
+  // Case 14: FCGI_WEB_SERVER_ADDRS is bound and empty. The descriptor is a
+  // valid socket.
+  {
+    ClearFCGIWebServerAddrs();
+    ValidSocketCase(AF_INET, 1, 1, EXIT_FAILURE, 14);
+  }
 
+  // Case 15: max_connections == std::numeric_limits<int>::max() &&
+  //          max_requests    == std::numeric_limits<int>::max()
+  // Also, a non-default value is provided for app_status_on_abort.
+  {
+    int max {std::numeric_limits<int>::max()};
+    ValidSocketCase(AF_INET, max, max, -10, 15);
+  }
 
+  auto UnixValidSocketCase = [](int test_case)->void
+  {
+    std::string case_suffix {" case "};
+    case_suffix += std::to_string(test_case);
+    case_suffix += ".";
 
+    // The Bazel temporary directory has a path which is too long for
+    // Unix domain sockets. This due to the struct sockaddr_un limit
+    // on the size of sun_path (92 bytes historical, 108 bytes currently).
+    std::string unix_socket_path {"/tmp/fcgi_si_test-Unix-socket_path"};
+    struct sockaddr_un unix_addr {};
+    unix_addr.sun_family = AF_UNIX;
+    strcpy(unix_addr.sun_path, unix_socket_path.data());
 
+    int socket_fd {socket(AF_UNIX, SOCK_STREAM, 0)};
+    if(socket_fd < 0)
+    {
+      ADD_FAILURE() << "A call to socket failed in" << case_suffix << '\n' 
+        << strerror(errno);
+    }
+    else
+    {
+      if(bind(socket_fd, 
+        static_cast<struct sockaddr*>(static_cast<void*>(&unix_addr)),
+        sizeof(struct sockaddr_un)) < 0)
+      {
+        ADD_FAILURE() << "A call to bind failed in" << case_suffix << '\n'
+          << strerror(errno);
+        close(socket_fd);
+      }
+      else
+      {
+        if(listen(socket_fd, 5) < 0)
+        { 
+          ADD_FAILURE() << "A call to listen failed in" << case_suffix << '\n' 
+            << strerror(errno);
+          close(socket_fd);
+          if(unlink(unix_socket_path.data()) < 0)
+            FAIL() << "The test Unix domain socket file could not be unlinked."
+              << '\n' << strerror(errno); 
+        }
+        else
+        {
+          EXPECT_NO_THROW(fcgi_si::FCGIServerInterface(socket_fd, 1, 1));
+          close(socket_fd);
+          if(unlink(unix_socket_path.data()) < 0)
+            FAIL() << "The test Unix domain socket file could not be unlinked."
+              << '\n' << strerror(errno);
+        }
+      }
+    }
+  };
 
+  // Case 16: A Unix-domain socket is used. FCGI_WEB_SERVER_ADDRS is unbound.
+  {
+    if(unsetenv("FCGI_WEB_SERVER_ADDRS") < 0)
+      ADD_FAILURE() << "FCGI_WEB_SERVER_ADDRS could not be removed from the "
+        "environment in case 16." << '\n' << strerror(errno);
+    else
+    {
+      UnixValidSocketCase(16);
+      ClearFCGIWebServerAddrs();
+    }
+  }
 
-
-
-
-
-
-
-
-
-
-
-
+  // Case 17: A Unix-domain socket is used. FCGI_WEB_SERVER_ADDRS is bound and
+  // has IPv4 address 127.0.0.1.
+  {
+    if(setenv("FCGI_WEB_SERVER_ADDRS", "127.0.0.1", 1))
+      ADD_FAILURE() << "A call to setenv failed in case 17." << '\n'
+        << strerror(errno);
+    else
+    {
+      UnixValidSocketCase(17);
+      ClearFCGIWebServerAddrs();
+    }
+  }
 }
